958bfe11eb75243e3627d0bcda9fde6d0a9e5acd
Noah <vik_026@mail.ru>
2022-03-24 19:30:41 +0300

    Task25

---
+++ Homework25/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите натуральное число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a != null)
+{
+    double x = Math.Pow(a, b);
+    Console.WriteLine(x);
+}
