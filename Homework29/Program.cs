ffd80e8ba1dc53397207f0ddb0dc9c99d247e9fa
Noah <vik_026@mail.ru>
2022-03-25 21:10:49 +0300

    Task29

---
+++ Homework29/Program.cs
@@ -0,0 +1,38 @@
+﻿void FillaArray (int [] collection)
+{
+    Random rand = new Random();
+    for(int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = rand.Next(-20, 30);
+    }
+}
+void PrintArray (int [] col)
+{
+    Console.WriteLine("Вывод массива:");
+    for(int i = 0; i < col.Length; i++)
+    {
+        Console.Write($"{col[i]}|");
+    }
+    Console.WriteLine();
+}
+
+void SortArray (int [] collection)
+{
+    for(int i = 0; i < collection.Length - 1; i++)
+    {
+        int minValue = i;
+        for(int j = i + 1; j < collection.Length; j++)
+        {
+            if(Math.Abs(collection[j]) < Math.Abs(collection [minValue])) minValue = j;
+        }
+        int temp = collection[i];
+        collection [i] = collection [minValue];
+        collection [minValue] = temp;
+    }
+}
+int [] array = new int [8];
+FillaArray(array);
+PrintArray(array);
+SortArray(array);
+Console.WriteLine("Сортировка массива: ");
+PrintArray(array);
