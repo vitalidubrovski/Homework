117a795efb829572ca95291d805475584a3f6d90
Noah <vik_026@mail.ru>
2022-04-02 20:22:44 +0300

    Task47

---
+++ Homework47/Program.cs
@@ -0,0 +1,29 @@
+﻿int rows = 5;
+int colums = 5;
+double[,] array = new double[rows, colums];
+FillArray();
+PrintArray();
+
+void FillArray()
+{
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            array[i, j] = rand.NextDouble() * 5;
+        }
+    }
+}
+void PrintArray()
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            Console.Write($"{array[i, j]}|");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
