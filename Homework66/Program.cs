315d5784605c7a6083e33e1cfa4ba917b0eaf21e
Noah <vik_026@mail.ru>
2022-04-09 21:22:08 +0300

    task66

---
+++ Homework66/Program.cs
@@ -0,0 +1,11 @@
+﻿int m = 1;
+int n = 15;
+int summ = 0;
+Recursion(m, n);
+void Recursion(int a, int b)
+{
+    if (a == b + 1) return;
+    summ += +a;
+    Recursion(a + 1, b);
+}
+Console.WriteLine(summ);
