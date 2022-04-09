d663ba1a32143e1e9c84dfcaf76c3a1cf04111dc
Noah <vik_026@mail.ru>
2022-04-09 20:41:27 +0300

    task64

---
+++ Homework64/Program.cs
@@ -0,0 +1,15 @@
+﻿int m = 1;
+int n = 33;
+Recursion(m, n);
+void Recursion(int a, int b)
+{
+    if (a == b + 1) return;
+    {
+        if (a % 3 == 0)
+        {
+            Console.Write(a + " ");
+        }
+
+    }
+    Recursion(a + 1, n);
+}
