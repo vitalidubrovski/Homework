c529d51360814b000d001132b90f072547eb0a9f
Noah <vik_026@mail.ru>
2022-04-10 12:12:50 +0300

    Task68

---
+++ Homework68/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = 4;
+int b = 2;
+int Ack(int n, int m)
+{
+    if (n < 0 || m < 0) throw new ArgumentOutOfRangeException();
+    if (n == 0) return m + 1;
+    if (m == 0) return Ack(n - 1, 1);
+    return Ack(n - 1, Ack(n, m - 1));
+}
+Console.WriteLine(Ack(a, b));
